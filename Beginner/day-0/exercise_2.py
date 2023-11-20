56a5e48e229b0c811faa7a357223dd668dc6e3f9
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-20 22:39:26 +0100

    exercises

---
+++ Beginner/day-0/exercise_2.py
@@ -0,0 +1,23 @@
+"""
+String Manipulation
+"""
+'''
+#Fix the code below 👇
+
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+'''
+
+# 1. Expected opening quotation mark
+print("Day 1 - String Manipulation")
+
+# 2. Expected string to start with single quotes so as to allow + to be printed
+print('String Concatenation is done with the "+" sign.')
+
+# 3. Indentation Error
+print('e.g. print("Hello " + "world")')
+
+# 4. Double Open Brackets
+print("New lines can be created with a backslash and n.")
---
+++ Beginner/day-0/exercise_3.py
@@ -0,0 +1,20 @@
+'''
+Write a program that prints the number of characters in a user's name.
+You might need to Google for a function that 
+calculates the length of a string.
+
+#### Example Input
+`Angela`
+
+#### Example Output
+`6`
+
+#### e.g. When you hit run, this is what should happen:
+
+#### Hint
+- You can put functions inside other functions.
+- Don't try to print anything other than the length.
+'''
+
+# SOLUTION
+print(len(input("Enter your name: ")))
