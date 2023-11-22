81dc1454d26a90e2d79b12b9aba3e1c4c3f8301e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-22 19:58:28 +0100

    math operators

---
+++ Beginner/day-1/math_operators.py
@@ -0,0 +1,37 @@
+"""
+Learning about the differnet math operators in Python
+"""
+
+
+'''
+# Addition
+>>> 2 + 2
+'''
+'''
+# subtraction
+>>> 2 - 2
+'''
+
+'''
+# Multiplication
+2 * 2
+'''
+
+'''
+# Division
+2 / 2  # division results in floating point number
+'''
+
+'''
+# Modulus
+2 % 2
+'''
+
+'''
+# Exponent
+2 ** 2
+'''
+
+# ALSO ENSURE TO BE AWARE OF PRIORITY OR ORDER OF PRIORITY AMONG OPERATORS
+# PEMDAS
+
