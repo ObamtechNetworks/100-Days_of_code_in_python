0fdafe3d81145cb290621734f5897d520ef2f30f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-03 05:38:28 +0100

    day 9

---
+++ Beginner/day-8/nesting_dictionaries.py
@@ -0,0 +1,12 @@
+travel_log = {
+    
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "meal_had": ["spinach", "salad"]}
+}
+
+# print(travel_log)
+
+# NESTINGA DICTIONARY INSTIDE A LIST
+travel_log- [
+	{"country"}
+]
