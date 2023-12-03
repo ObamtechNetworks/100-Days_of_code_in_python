aed60c158a48d50000c16aaa23752259c560f1b1
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-12-03 13:39:35 +0100

    can still be improved

--- Beginner/day-8/auction_program.py
+++ Beginner/day-8/auction_program.py
@@ -1,14 +1,25 @@
 from os import system
 from art import logo
+import sys
 #HINT: You can call clear() to clear the output in the console.
 # os.clear()
 
 
 def highest_bidder(bidding_data):
     # get max bidder
+    
     max_bidder = max(bidding_data, key=bidding_data.get)
     max_bid = bidding_data[max_bidder]
+    # going the crude way
+    '''
+    max_bidder = None
+    max_bid = -1
     
+    for key, value in bidding_data.items():
+        if value > max_bid:
+            max_bid = value
+            max_bidder = key
+    '''
     return [max_bidder, max_bid]
 
 # print logo
@@ -18,7 +29,11 @@ game_play = True
 while game_play:
     print("Welcome to the secret auction program.")
     user_name = input ("What is your name?: ")
-    bid_value = int(input ("What's your bid?: $"))
+    try:
+    	bid_value = int(input ("What's your bid?: $"))
+    except Exception:
+        print("Bid value must be a number")
+        sys.exit(1)
     other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.")
     # store data into dictionary
     bidding_info[user_name] = bid_value
